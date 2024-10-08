0e3b5858c07085a4b1570cd19ff75f85aec95812
gabrielvidigalf <gabrielcvfcvf@gmail.com>
2024-10-07 21:37:14 -0300

    Implementa métodos CRUD: get, post, put e delete na classe Client

--- src/Client.js
+++ src/Client.js
@@ -7,12 +7,36 @@ class Client extends Api {
     this.path = `${baseURL}api/v1/clients`
   }
 
+  async getAllClients() {
+    const endpoint = this.path; 
+    return await this.get(endpoint);
+  }
+
   // Método que utiliza o GET para obter dados
-  async getData(id = null) {
+  async getClientById(id = null) {
     let endpoint = id ? `/${id}` : '';
     endpoint = `${this.path}${endpoint}`;
 
     return await this.get(endpoint);
+  }
+
+  async createCliente() { // preciso passar o "data" como parametro aqui?
+    const endpoint = this.path; 
+    return await this.post(endpoint);
+  }
+
+  async updateCliente(id = null) {
+    let endpoint = id ? `/${id}` : '';
+    endpoint = `${this.path}${endpoint}`;
+
+    return await this.post(endpoint);
+  }
+
+  async deleteCliente(id = null) {
+    let endpoint = id ? `/${id}` : '';
+    endpoint = `${this.path}${endpoint}`;
+
+    return await this.delete(endpoint);
   }
 
 }
