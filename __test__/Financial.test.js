e9273ef2974c117dd3452c91ca018a9d6415cbf0
Luiz Fernando <112594037+LuizFernandoFerri@users.noreply.github.com>
2024-10-11 16:08:06 -0300

    atualizando mock

--- __test__/Financial.test.js
+++ __test__/Financial.test.js
@@ -5,7 +5,7 @@ const Api = require("../src/Api.js");
 // Mock da classe Api
 jest.mock("../src/Api.js");
 
-describe("Event Class", () => {
+describe("Movimentacao Class", () => {
   let event;
   const baseURL = process.env.ME_BASE_URL;
   const apiKey = process.env.ME_API_KEY;
@@ -15,19 +15,19 @@ describe("Event Class", () => {
     // Mockar implementações dos métodos get e post
     Api.prototype.get = jest.fn();
     Api.prototype.post = jest.fn();
-    // Instanciar a classe Event
+    // Instanciar a classe Financial
     event = new Event(baseURL, apiKey);
   });
   describe("list method", () => {
     it("deve chamar o método get com o endpoint correto sem ID", async () => {
-      const mockResponse = { id: 1, nome: "Evento 1" };
+      const mockResponse = { id: 1, nome: "Movimentacao 1" };
       Api.prototype.get.mockResolvedValue(mockResponse);
       const response = await event.list();
       expect(Api.prototype.get).toHaveBeenCalledWith(`${baseURL}api/v1/financial`);
       expect(response).toEqual(mockResponse);
     });
     it("deve chamar o método get com o endpoint correto com ID", async () => {
-      const mockResponse = { id: 1, nome: "Evento 1" };
+      const mockResponse = { id: 1, nome: "Movimentacao 1" };
       Api.prototype.get.mockResolvedValue(mockResponse);
       const id = 1;
       const response = await event.list(id);
