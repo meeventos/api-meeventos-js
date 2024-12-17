d58a9ba57fd0dafbcea18928a35a6e84de38559a
Luiz <luiz@localhost.localdomain>
2024-12-17 10:15:25 -0300

    Alteração e ajuste do arquivo financial

--- src/Financial.js
+++ src/Financial.js
@@ -3,45 +3,15 @@ const Api = require("./Api");
 class Financial extends Api {
   constructor(baseURL, apiKey) {
     super(baseURL, apiKey);
-
     this.path = `${baseURL}api/v1/financial`;
   }
 
-  // Método que utiliza o GET para obter dados
-  async list(id = null, options = {}) {
-    let endpoint = id ? `/${id}` : '';
+  // Lista todos Financeiro ou exibe uma Movimentação de um determinado ID
+  async list(id = null, json = {}) {
+    let params = new URLSearchParams(json).toString();
+    let endpoint = id ? `/${id}` : json ? `?${params}` : "";
+
     endpoint = `${this.path}${endpoint}`;
-
-    // Converter opções em string de consulta
-    const queryParams = new URLSearchParams();
-
-    // Adicionar parâmetros de ordenação
-    if (options.sort) queryParams.append('sort', options.sort);
-    if (options.field_sort) queryParams.append('field_sort', options.field_sort);
-
-    // Adicionar parâmetros de período
-    if (options.start) queryParams.append('start', options.start);
-    if (options.end) queryParams.append('end', options.end);
-
-    // Adicionar parâmetros de paginação
-    if (options.page) queryParams.append('page', options.page);
-    if (options.limit) queryParams.append('limit', options.limit);
-
-    // Adicionar parâmetro de busca
-    if (options.search) queryParams.append('search', options.search);
-
-    // Adicionar quaisquer outros parâmetros personalizados
-    for (const [key, value] of Object.entries(options)) {
-      if (!['sort', 'field_sort', 'start', 'end', 'page', 'limit', 'search'].includes(key)) {
-        queryParams.append(key, value);
-      }
-    }
-
-    // Anexar a string de consulta à URL, se houver parâmetros
-    const queryString = queryParams.toString();
-    if (queryString) {
-      endpoint += `?${queryString}`;
-    }
 
     return await this.get(endpoint);
   }
